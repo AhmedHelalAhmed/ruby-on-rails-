a526121d468e5f306b37a32e99eb4616af837cd5
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-05-01 12:57:18 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:edit, :update]
+  resources :users, except: [:delete]
+
+  match '/users','users#index'
+  match '/users/:id','users#show', :get # i take parameter id
+  #match '/users/id','users#show' #i wait id word
+  post '/users','users#create', as: :give_me_users_create
+  get '/users/new','users#create', as: :direct_me_to_new_user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #regular expression >>> custom route
-  #
+  #this will generate 7 routes
 end
